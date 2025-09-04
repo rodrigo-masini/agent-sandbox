2d61f09e3821933c32a5b53cc8e2c56d7e904771
rodrigo-masini <rodrigo@researchmagic.com>
2025-09-04 17:17:58 -0300

    Update index.php

--- backend/public/index.php
+++ backend/public/index.php
@@ -5,7 +5,7 @@
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-use Pandora\Core\Application;
+use Agtsdbx\Core\Application;
 
 // Enable error reporting in development
 if (($_ENV['APP_ENV'] ?? 'development') === 'development') {
